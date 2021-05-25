9a2540891e9f2ad1d60a70c8d05d0cac4fe122f6
Your Name <prashanth726@gmail.com>
2021-05-25 08:33:52 +0530

    added a single todo to todos/:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,15 @@
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    todo = Todo.find_by(id: id)
+    if todo.nil?
+      render plain: "Todo not found, please check the todo id"
+    else
+      render plain: todo.to_pleasant_string
+    end
   end
 end
