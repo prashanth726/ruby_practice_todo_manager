f1426f6f4eb5ebb58972092c3584b8cb7c2a6ed2
Your Name <prashanth726@gmail.com>
2021-05-25 09:00:13 +0530

    added a create route for creating new route

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,4 +12,11 @@ class TodosController < ApplicationController
       render plain: todo.to_pleasant_string
     end
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+    render plain: "Hey the new todo is created #{new_todo.id}"
+  end
 end
