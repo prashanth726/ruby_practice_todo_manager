f44dd7be6767e1004ed48c8de9c192109df0f89a
Your Name <prashanth726@gmail.com>
2021-06-03 08:53:22 +0530

    rendered todos with partials based on todo status

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,29 @@
+require "active_record"
+
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def due_today?
+    @due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = find_by(id: todo_id)
+    todo.update(completed: true)
+    todo
+  end
+
+  def self.completed
+    all.where(completed:true)
   end
 end
